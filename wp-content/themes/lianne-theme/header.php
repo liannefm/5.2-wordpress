2ac5eec3d1b04944dbf54abd4324943097ec387e
liannefm <lfmeiresonne@gmail.com>
2025-10-26 18:08:51 +0100

    dikke updateeee

--- wp-content/themes/lianne-theme/header.php
+++ wp-content/themes/lianne-theme/header.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="icon" href="<?php echo esc_url( get_site_icon_url() ); ?>" sizes="32x32" />
 </head>
 <body>
 <header>
-    <h1><?php bloginfo('name'); ?></h1>
-    <p><?php bloginfo('description'); ?></p>
+    <img class="logo" src="<?php echo esc_url( get_site_icon_url() ); ?>">
 
-    <nav>
-        <?php wp_nav_menu(array('theme_location' => 'hoofdmenu')); ?>
-    </nav>
+    <div class="main">
+        <h1><?php bloginfo('name'); ?></h1>
+
+        <nav>
+            <?php wp_nav_menu(array('theme_location' => 'hoofdmenu')); ?>
+        </nav>
+    </div>
 </header>
 
--- wp-content/themes/lianne-theme/index.php
+++ wp-content/themes/lianne-theme/index.php
@@ -1,18 +1,73 @@
-<?php get_header(); ?>
-<div id="content">
-    <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-            ?>
-            <h2><?php the_title(); ?></h2>
-            <div><?php the_content(); ?></div>
-            <?php
-        endwhile;
-    else :
-        echo '<p>Geen berichten gevonden.</p>';
-    endif;
-    ?>
+<?php 
+$recent_posts = new WP_Query([
+    'post_type'      => 'konijnen-informatie',
+    'orderby'        => 'date',
+    'order'          => 'DESC'
+]);
 
-</div>
+$recent_post = new WP_Query([
+    'post_type'      => 'konijnen-informatie',
+    'posts_per_page' => 1,
+    'orderby'        => 'date',
+    'order'          => 'DESC'
+]);
+
+get_header(); 
+?>
+<main>
+    <div class="hero" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_template_directory_uri(); ?>/assets/images/konijnenplat.jpg');">
+        <div class="text">
+            <h1>Konijnen content</h1>
+            <p>Regelmatig schrijf ik blogs om mijn kennis en ervaring met jou te delen. Ze zitten bomvol tips & adviezen over gedrag, verzorging, huisvesting, voeding en gezondheid.</p>
+        </div>
+    </div>
+
+    <div class="recent-blog">
+        <h2>Laaste blog</h2>
+
+        <?php 
+        if ($recent_post->have_posts()) :
+            while ($recent_post->have_posts()) :
+                $recent_post->the_post();
+                ?>
+                <a href="<?php the_permalink(); ?>" class="blog">
+                    <img src="<?php echo get_field('afbeelding')['url'] ?>">
+
+                    <div class="info">
+                        <p class="title"><?php the_title(); ?></p>
+                        <?php the_content(); ?>
+                    </div>
+                </a>
+                <?php
+            endwhile;
+        endif;
+        ?>
+    </div>
+
+    <div class="recent-blogs">
+        <h2>Mijn recente blogs</h2>
+
+        <div class="blogs">
+            <?php 
+            if ($recent_posts->have_posts()) :
+                while ($recent_posts->have_posts()) :
+                    $recent_posts->the_post();
+                    ?>
+
+                    <a href="<?php the_permalink(); ?>" class="blog">
+                        <img src="<?php echo get_field('afbeelding')['url'] ?>">
+                        <div class="info">
+                            <p class="title"><?php the_title(); ?></p>
+                            <p class="timestamp"><?php echo get_the_date('F j, Y') ?></p>
+                        </div>
+                    </a>
+
+                    <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
+    </div>
+</main>
 <?php get_footer(); ?>
 
